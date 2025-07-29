b8531e1d91eea390ae06829d45f7f72935407245
Stephen Dawes <stephenrdawes@gmail.com>
2025-07-29 17:42:02 +0100

    Initial Commit

---
+++ PlainTextMoney/PlainTextMoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlainTextMoneyApp.swift
+//  PlainTextMoney
+//
+//  Created by Stephen Dawes on 29/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct PlainTextMoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
